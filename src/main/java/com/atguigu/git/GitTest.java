d6d7fce56dc08300f167a95a3f3b64c7d992a694
SandyStart <1551222388@qq.com>
2021-08-20 10:39:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ZhangQuan
+ * @create 2021-08-20 10:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
